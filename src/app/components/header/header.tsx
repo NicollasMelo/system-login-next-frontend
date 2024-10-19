120b29f874bc3d108627bca2c4220df4c00bfe4f
NicollasMelo <nicollasmelo2001@gmail.com>
2024-10-19 18:21:51 -0300

    first-commit, iniciando roteamento

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <p>Sou o header</p>
+    </div>
+  );
+}
---
+++ src/app/pages/header/page.tsx
@@ -0,0 +1,9 @@
+import Header from '@/app/components/header/header';
+
+export default function HeaderPage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
