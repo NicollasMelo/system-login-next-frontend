26701c4f5b85f7ee36551e3a8fbbcf1778cc861e
NicollasMelo <nicollasmelo2001@gmail.com>
2024-10-20 19:52:23 -0300

    feat: criando tela de criacao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import Content from './components/content/content';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
-import ContentPage from './pages/content/page';
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <ContentPage />
+      <Content />
       <Footer />
     </div>
   );
---
+++ src/app/pages/create/page.tsx
@@ -0,0 +1,11 @@
+import Create from '@/app/components/create/create';
+import Header from '@/app/components/header/header';
+
+export default function CreatePage() {
+  return (
+    <>
+      <Header />
+      <Create />
+    </>
+  );
+}
